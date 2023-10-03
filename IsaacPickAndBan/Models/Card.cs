62afe917272af9a757148fb978ffe1d123115a0a
Florian Grazina <114757171+Florian-Grazina@users.noreply.github.com>
2023-10-03 17:04:57 +0200

    create json list of cards

--- IsaacPickAndBan/Models/Card.cs
+++ IsaacPickAndBan/Models/Card.cs
@@ -4,18 +4,18 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public Extension Extension { get; set; }
+        public string Extension { get; set; }
         public string Image { get; set; }
         public string Ethernal { get; set; }
 
 
-        public Card(int id, string name, Extension extension, string ethernal)
+        public Card(int id, string name, string extension, string ethernal)
         {
             Id = id;
             Name = name;
             Extension = extension;
             Image = $"{extension}_{name}.png";
-            Ethernal = $"{ethernal}_{name}.png";
+            Ethernal = $"{extension}_{ethernal}.png";
         }
     }
 }
--- IsaacPickAndBan/ViewModels/MainViewModel.cs
+++ IsaacPickAndBan/ViewModels/MainViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using IsaacPickAndBan.Models;
+using Microsoft.Maui.Storage;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace IsaacPickAndBan.ViewModels
 {
@@ -45,45 +48,8 @@ namespace IsaacPickAndBan.ViewModels
 
         public void InitMainPage()
         {
-            ListOfCards = new List<Card>()
-            {
-                new Card(1, "isaac", Extension.b2, "the_d6"),
-                new Card(2, "maggy", Extension.b2, "yum_heart"),
-                new Card(3, "cain", Extension.b2, "sleight_of_hand"),
-                new Card(4, "judas", Extension.b2, "book_of_belial"),
-                new Card(5, "isaac", Extension.b2, "the_d6"),
-                new Card(6, "maggy", Extension.b2, "yum_heart"),
-                new Card(7, "cain", Extension.b2, "sleight_of_hand"),
-                new Card(8, "judas", Extension.b2, "book_of_belial"),
-                new Card(9, "isaac", Extension.b2, "the_d6"),
-                new Card(10, "maggy", Extension.b2, "yum_heart"),
-                new Card(11, "cain", Extension.b2, "sleight_of_hand"),
-                new Card(12, "judas", Extension.b2, "book_of_belial"),
-                new Card(13, "isaac", Extension.b2, "the_d6"),
-                new Card(14, "maggy", Extension.b2, "yum_heart"),
-                new Card(15, "cain", Extension.b2, "sleight_of_hand"),
-                new Card(16, "judas", Extension.b2, "book_of_belial"),
-                new Card(17, "isaac", Extension.b2, "the_d6"),
-                new Card(18, "maggy", Extension.b2, "yum_heart"),
-                new Card(19, "cain", Extension.b2, "sleight_of_hand"),
-                new Card(20, "judas", Extension.b2, "book_of_belial"),
-                new Card(21, "isaac", Extension.b2, "the_d6"),
-                new Card(22, "maggy", Extension.b2, "yum_heart"),
-                new Card(23, "cain", Extension.b2, "sleight_of_hand"),
-                new Card(24, "judas", Extension.b2, "book_of_belial"),
-                new Card(25, "isaac", Extension.b2, "the_d6"),
-                new Card(26, "maggy", Extension.b2, "yum_heart"),
-                new Card(27, "cain", Extension.b2, "sleight_of_hand"),
-                new Card(28, "judas", Extension.b2, "book_of_belial"),
-                new Card(29, "isaac", Extension.b2, "the_d6"),
-                new Card(30, "maggy", Extension.b2, "yum_heart"),
-                new Card(31, "cain", Extension.b2, "sleight_of_hand"),
-                new Card(32, "judas", Extension.b2, "book_of_belial"),
-                new Card(33, "isaac", Extension.b2, "the_d6"),
-                new Card(34, "maggy", Extension.b2, "yum_heart"),
-                new Card(35, "cain", Extension.b2, "sleight_of_hand"),
-                new Card(36, "judas", Extension.b2, "book_of_belial")
-            };
+            string cardsJson = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "cards.json");
+            ListOfCards = JsonSerializer.Deserialize<List<Card>>(cardsJson);
         }
     }
 }
