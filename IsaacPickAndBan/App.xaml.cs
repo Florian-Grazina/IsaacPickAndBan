dc7269d2953d48b38f26078b63190c0d29f93ae5
Florian Grazina <floriangrazina@gmail.com>
2025-07-15 16:41:53 +0200

    rename main page

--- IsaacPickAndBan/App.xaml.cs
+++ IsaacPickAndBan/App.xaml.cs
@@ -6,29 +6,29 @@ namespace IsaacPickAndBan
     public partial class App : Application
     {
         private readonly Data _data;
-        private Page _startPage;
+        private Page _mainPage;
 
         public App(Data data)
         {
             InitializeComponent();
             _data = data;
 
-            _startPage = new LoadingPage();
+            _mainPage = new LoadingPage();
             InitializeApp();
         }
 
         private async void InitializeApp()
         {
             await _data.InitializeAsync();
-            _startPage = new AppShell();
+            _mainPage = new AppShell();
 
             if (Current?.Windows[0] is Window window)
-                window.Page = _startPage;
+                window.Page = _mainPage;
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(_startPage);
+            return new Window(_mainPage);
         }
     }
 }
